d4146de5b2281c29edeaa42eb16428945a6fb56f
flaw5 <flaw561@gmail.com>
2021-04-18 17:19:26 +0600

    pages, controller, model edited

--- src/main/java/ru/omsu/imit/web/controller/MainController.java
+++ src/main/java/ru/omsu/imit/web/controller/MainController.java
@@ -1,5 +1,6 @@
 package ru.omsu.imit.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
@@ -10,10 +11,12 @@ import ru.omsu.imit.web.utils.WebUtils;
 import java.security.Principal;
 
 @Controller
+@Slf4j
 public class MainController {
 
     @GetMapping(value = { "/", "/welcome" })
     public String welcomePage(Model model) {
+        log.debug("/welcome page is accessed");
         model.addAttribute("title", "Welcome");
         model.addAttribute("message", "This is welcome page!");
         return "welcomePage";
@@ -21,29 +24,34 @@ public class MainController {
 
     @GetMapping(value = "/admin")
     public String adminPage(Model model, Principal principal) {
+        log.debug("/admin page is accessed");
         addUserInfoAttribute(model, principal);
         return "adminPage";
     }
 
     @GetMapping(value = "/login")
     public String loginPage() {
+        log.debug("/login page is accessed");
         return "loginPage";
     }
 
     @GetMapping(value = "/logoutSuccessful")
     public String logoutSuccessfulPage(Model model) {
+        log.debug("/logoutSuccessful page is accessed");
         model.addAttribute("title", "Logout");
         return "logoutSuccessfulPage";
     }
 
     @GetMapping(value = "/userInfo")
     public String userInfo(Model model, Principal principal) {
+        log.debug("/userInfo page is accessed");
         addUserInfoAttribute(model, principal);
         return "userInfoPage";
     }
 
     @GetMapping(value = "/403")
     public String accessDenied(Model model, Principal principal) {
+        log.debug("/403 page is accessed");
         if (principal != null) {
             addUserInfoAttribute(model, principal);
             String message = "Hi " + principal.getName() //
